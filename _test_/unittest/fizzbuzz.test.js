7e7900086c7274767877db48fce1dc91083dc9f8
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-04-29 16:10:47 +0200

    changes in fizzbuzz test file

--- _test_/unittest/fizzbuzz.test.js
+++ _test_/unittest/fizzbuzz.test.js
@@ -1,22 +1,36 @@
 // Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
 import { describe, it, expect } from "vitest";
-import { fizzBuzz } from "../../src/unitJS/fizzbuzz.js";
+import  fizzbuzz from "../../src/unitJS/fizzbuzz.js";
 
-describe("fizzBuzz", () => {
-  it("returns 'Fizz' for multiples of 3", () => {
-    expect(fizzBuzz(3)).toBe("Fizz");
-  });
-
-  it("returns 'Buzz' for multiples of 5", () => {
-    expect(fizzBuzz(5)).toBe("Buzz");
-  });
-
-  it("returns 'FizzBuzz' for multiples of both 3 and 5", () => {
-    expect(fizzBuzz(15)).toBe("FizzBuzz");
-  });
-
-  it("returns the number for non-multiples", () => {
-    expect(fizzBuzz(7)).toBe(7);
-  });
+describe("fizzbuzz for number 1-100", () => {
+  for (let i = 1; i <= 100; i++) {
+    const expected = (i % 3 === 0 && i % 5 === 0) ? "FizzBuzz" :
+                     (i % 3 === 0) ? "Fizz" :
+                     (i % 5 === 0) ? "Buzz" : 
+                     String(i)
+  
+    it(`returns ${expected} for ${i}`, () => {
+      expect(fizzbuzz(i)).toBe(expected);
+    });
+  }
 });
+
+
+
+//   it("returns 'Fizz' for multiples of 3", () => {
+//     expect(fizzbuzz(3)).toBe("Fizz");
+//   });
+
+//   it("returns 'Buzz' for multiples of 5", () => {
+//     expect(fizzbuzz(5)).toBe("Buzz");
+//   });
+
+//   it("returns 'FizzBuzz' for multiples of both 3 and 5", () => {
+//     expect(fizzbuzz(15)).toBe("FizzBuzz");
+//   });
+
+//   it("returns the number for non-multiples", () => {
+//     expect(fizzbuzz(7)).toBe(7);
+//   });
+// });
--- src/unitJS/fizzbuzz.js
+++ src/unitJS/fizzbuzz.js
@@ -1,9 +1,11 @@
 // Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
-const fizzBuzz = (n) => {
-    if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";  
-    if (n % 3 === 0) return "Fizz";
-    if (n % 5 === 0) return "Buzz";
-    return n;
-};
-export { fizzBuzz };
+const fizzbuzz = (n) => {
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
+  return String(n);
+};      
+
+
+export default fizzbuzz;
